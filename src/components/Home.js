b2fcc209b61c370dafa1d2f18457f2193c868e7e
Kiran <kiranmandapaka@fisecglobal.net>
2025-05-12 12:56:39 +0530

    phone number

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
       <div className="home-content">
         <h1 className="lead-content">Hi, I'm Kiran Mandapaka</h1>
         <p className="lead-content-h2">Java Developer | React Enthusiast | Problem Solver</p>
-        <a href="https://drive.google.com/file/d/1WVtBqv_F6gfQIST6LJh9fxA5A3dICEup/view?usp=drive_link" className="btn">Download Resume</a>
+        <a href="https://drive.google.com/file/d/1HLAt_3V4KlkazNRDQqsEIx4T-OsH-ttS/view?usp=sharing" className="btn">Download Resume</a>
       </div>
       </div>
     </div>
